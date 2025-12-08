14e9d35969ed28d7db4fcc4af0df1e2c90a30537
Richeles <richeles32@gmail.com>
2025-12-08 17:53:06 -0300

    UltraDeploy AutoCommit 2025-12-08 17:53:05

---
+++ app/rebanho/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function RebanhoPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">🐄 Rebanho</h1>
+      <p>Carregando dados...</p>
+    </div>
+  );
+}
